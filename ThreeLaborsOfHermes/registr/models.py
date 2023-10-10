95d8fe3359fc01383d4637cef4bf43692fc3e0f6
Andrii Druzhha <113832819+GKAndrey@users.noreply.github.com>
2023-10-10 23:16:15 +0300

    Go to django

---
+++ ThreeLaborsOfHermes/registr/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+# class User(models.Model):
+#     login = models.CharField(max_length=255)
+#     password = models.CharField(max_length=255)
+#     emeil = models.CharField(max_length=255)
+#     log_ip = models.CharField(max_length=255, blank=True, null=True)
